0b7d84ce49c7e212e32d92159e45edabaa3c1ce0
india <india@user>
2018-09-05 14:44:01 +0530

    2nd commit

--- src/com/app/Employee.java
+++ src/com/app/Employee.java
@@ -2,5 +2,24 @@ package com.app;
 
 public class Employee {
 	private int a;
+	private String name;
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Employee [a=" + a + ", name=" + name + "]";
+	}
+	
+	
 
 }
